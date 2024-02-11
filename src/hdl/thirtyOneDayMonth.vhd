6842349cce839117e711ceab80c16a3f980d0831
pnunn15 <69120932+pnunn15@users.noreply.github.com>
2024-02-11 14:48:19 -0700

    Finished Basys3 implementation

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -55,25 +55,35 @@ library ieee;
 -- entity name should match filename, this has been filled out for you  
 entity thirtyOneDayMonth is 
   port(
-	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+    i_A : in std_logic;
+    i_B : in std_logic;
+    i_C : in std_logic;
+    i_D : in std_logic;
+    o_Y : out std_logic
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-	
+	signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
 	--signals internal to the architecture are declared and initialized such as w_sel
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
+	o_Y <= i_A when (w_sel = "000") else
+	       i_A when (w_sel = "001") else
+	       i_A when (w_sel = "010") else
+	       i_A when (w_sel = "011") else
+	       not i_A when (w_sel = "100") else
+           not i_A when (w_sel = "101") else
+           not i_A when (w_sel = "110") else
+           not i_A when (w_sel = "111") else
+           '0';
 	--finish assigning signals
-	
+	w_sel(0) <= i_B;
+	w_sel(1) <= i_C;
+	w_sel(2) <= i_D;
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
